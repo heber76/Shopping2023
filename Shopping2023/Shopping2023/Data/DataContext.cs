9c06a7de059afbcbafd4f42e0c8aba651c749f11
heber ochoa prieto <heber76@hotmail.com>
2023-01-13 11:49:02 -0600

    Create DB  && Country Entity && Add Countries Controller

---
+++ Shopping2023/Shopping2023/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Shopping2023.Data.Entities;
+
+namespace Shopping2023.Data
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(DbContextOptions<DataContext> options): base(options)
+        {
+        }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+
+    }
+}
---
+++ Shopping2023/Shopping2023/Data/Entities/Country.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shopping2023.Data.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage ="El campo {0} es obligatorio.")]
+        [MaxLength(50,ErrorMessage ="El campo {0} debe tener maximo {1} caracteres.")]
+        [Display(Name="País")]
+        public string Name { get; set; }
+
+
+    }
+}
